3e4515deda469681a21477c5535e7a14152165b2
Paul Phillips <contact@paulphillips.guru>
2025-01-27 18:44:45 +1100

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# FireNote
-A note taker app designed for Fire Firefighters
+# Fire Note
+
+The core goal of Fire Note is to allow Firefighters to log and recall notes that related to fighting fires or related activities. Though this app will never or should replace paper and pen as technology will never be 100% fall proof, it will provide a useful tool to aid with recording of notes and activities.
+
+I don't know about you, for me, I can write some important notes down on paper and come back couple of later thinking some kind of drunk stole wrote on my pad, only to find it was me :).
+
+The key features of Fire Note:
+
+1. Record activities related to a fire scene.
+2. Works completely offline, no internet is required 
+3. Real time 6 Grid reference shown with current heading and resection support to aid with finding current location.
+4. If online, nearest address will be shown based on current location
+5. Current location, heading, direction and last known address recorded 
+
+## Initial Screen
+
+<img src="C:\Paul\Projects\FireNoteApp\assets\Home.jpg" alt="Home" style="zoom:25%;" />
+
+1. 6 Digit Reference
+2. Heading 
+3. Resection Heading
+4. Home
+5. Add new incident
+
+## Adding a note
+
--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,7 +1,63 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),  VitePWA({
+    registerType: 'autoUpdate', // Auto-updates the service worker
+    devOptions: {
+      enabled: true, // Enable PWA in development
+    },
+    workbox: {
+      runtimeCaching: [
+        {
+          urlPattern: ({ request }) => request.destination === 'document',
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'html-cache',
+          },
+        },
+        {
+          urlPattern: ({ request }) => request.destination === 'script',
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'js-cache',
+          },
+        },
+        {
+          urlPattern: ({ request }) => request.destination === 'style',
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'css-cache',
+          },
+        },
+        {
+          urlPattern: ({ request }) => request.destination === 'image',
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'image-cache',
+          },
+        },
+      ],
+    },
+    manifest: {
+      name: 'Fire Note',
+      short_name: 'FireNote',
+      description: 'Fire Note for Fire Fighters',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: '/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+  })],
 })
